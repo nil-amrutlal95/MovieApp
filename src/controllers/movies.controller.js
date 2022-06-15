d3f305fad3abad52f2841ae0fbdf4d44b22ef06d
Nil Amrutlal <nil.amrutlal@procensus.pt>
2022-06-15 10:43:01 +0200

    Define controllers and validators

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -2,6 +2,8 @@ var MovieService = require("../services/movies.service");
 
 class MovieController {
 
+    static movieService = new MovieService();
+
     static searchMovieByKeyword = async (req, res) => {
 
         if(req.params.search === undefined){
@@ -9,18 +11,25 @@ class MovieController {
             return;
         }
 
-        const data = await MovieService.searchByKeyword(req.params.search, req.query.lang, req.query.page);
-        res.status(200).send(data);
+        if(req.query.sortBy && req.query.sortBy !== "id"  && req.query.sortBy !== "title" ) {
+            res.status(400).send({ error: true, message: "Invalid sort"});
+            return;
+        }
+
+
+        const data = await this.movieService.searchByKeyword(req.params.search, req.query.sortBy, req.query.lang, req.query.page);
+        res.status(200).send({error : false, data});
     }
 
 
     static searchMovieById = async (req, res) => {
         
         if(req.params.id === null || !Number(req.params.id)?true:false) {
-            console.log("null");
+            res.status(400).send({error: true, message: "Incorrect ID format"});
         }
 
-        return req.params.id;
+        const data = await this.movieService.searchById(req.params.id);
+        return res.status(200).send(data);
     }
 
 }
