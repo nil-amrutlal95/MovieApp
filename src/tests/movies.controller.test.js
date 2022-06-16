e2e787239eeb16f29d75c00a3478c7d0ca978363
Nil Amrutlal <nil.amrutlal@procensus.pt>
2022-06-16 16:04:10 +0200

    Added Jest Dependency and test file init

---
+++ src/tests/movies.controller.test.js
@@ -0,0 +1,11 @@
+const MovieController = require('../controllers/movies.controller');
+
+describe('Movie Controller : search by ID', () => {
+    test('Controller Validators', async () => {
+        const mockRequest = {};
+        mockRequest.params.query = {
+            
+        };
+    })
+
+})
---
+++ src/tests/movies.service.test.js
@@ -0,0 +1,12 @@
+const MovieService = require('../services/movies.service');
+
+const movieService = new MovieService();
+
+describe('MovieService: Search by keyword', () => {
+    test
+})
+
+
+describe('MovieService: Search by ID', () => {
+    test
+})
